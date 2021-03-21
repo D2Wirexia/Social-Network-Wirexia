e37f821c583beca15e7f08f474ebd20608f6766b
Seshchenko Dmitrii <DmitryW1Seschenko@ukr.net>
2021-03-21 21:44:20 +0200

    Updates

---
+++ precache-manifest.e5ded512421afa104d228b9eaaa3b64f.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47cc0664698e5b683fda0eff11a87f90",
+    "url": "/task-react/index.html"
+  },
+  {
+    "revision": "13a6023b9f5ce31fdc4c",
+    "url": "/task-react/static/css/main.a6f36e5e.chunk.css"
+  },
+  {
+    "revision": "4e12439cb3d64396a6b5",
+    "url": "/task-react/static/js/2.82f4acc0.chunk.js"
+  },
+  {
+    "revision": "4f2dedb9f87ffe36ce515bb1189ae74f",
+    "url": "/task-react/static/js/2.82f4acc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a6023b9f5ce31fdc4c",
+    "url": "/task-react/static/js/main.3a6c8f17.chunk.js"
+  },
+  {
+    "revision": "6a2e9b57003793760fd2",
+    "url": "/task-react/static/js/runtime-main.c5247d63.js"
+  },
+  {
+    "revision": "36578126e993ea8909308c633554168a",
+    "url": "/task-react/static/media/1.36578126.jpg"
+  },
+  {
+    "revision": "74336e199e10b0fa4317b6e31534bc87",
+    "url": "/task-react/static/media/dimuch.74336e19.png"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/task-react/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/task-react/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/task-react/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/task-react/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/task-react/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/task-react/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/task-react/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/task-react/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/task-react/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/task-react/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/task-react/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/task-react/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/task-react/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/task-react/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/task-react/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "3f2590067056b4f0630d5b360e694fce",
+    "url": "/task-react/static/media/loading.3f259006.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/task-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df94f53765c418a2e7bf06add9aa33fb",
+    "url": "/task-react/static/media/myAva.df94f537.jpg"
+  },
+  {
+    "revision": "2f7acb55d2b75d690ea5a62369cd4547",
+    "url": "/task-react/static/media/snw.2f7acb55.png"
+  }
+]);
